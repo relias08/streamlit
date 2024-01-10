c431772e6467a75f93c9724f607766bfb3016455
relias08 <relias08@gmail.com>
2024-01-10 14:35:03 +0530

    Update Fundus_Classfn___no_REST_API_end_point___app.py

--- Fundus_Classfn___no_REST_API_end_point___app.py
+++ Fundus_Classfn___no_REST_API_end_point___app.py
@@ -127,7 +127,7 @@ best_model = Classifier(model, lr=2e-5).to(device)       # so best_model is a Py
 # (this model can be used only to run INFERENCE using the trained model ie. not for continued training from saved checkpoint)
 
 checkpoint_path = "/content/gdrive/MyDrive/Colab Notebooks/_CNN___Main/___1___ViT/tb_logs123/Test___June_19/version_3/checkpoints/epoch=15-step=880.ckpt"
-checkpoint = torch.load(checkpoint_path)   
+checkpoint = torch.load(checkpoint_path, map_location = device)   
 best_model.load_state_dict(checkpoint['state_dict'])
 
 # ---------------------------------------------------------------------------------
